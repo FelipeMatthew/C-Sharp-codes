63dad99b9d2a3c65f4a1971d9adbe79bd1caf32a
Felipe Matthew Moreira Nascimento <102431464+FelipeMatthew@users.noreply.github.com>
2022-09-08 21:53:16 -0300

    Add files via upload

---
+++ Forms produto.cs
@@ -0,0 +1,17 @@
+﻿public class Produto
+{
+    public int id;
+    public string nome;
+    public int qtd_estoque;
+    public float preco;
+
+    // Método construtor - Com parametros
+    public Produto (int p_id,string p_nome,int p_qtd_estoque, float p_preco)
+    {
+        this.id = p_id;
+        this.nome = p_nome;
+        this.qtd_estoque = p_qtd_estoque;
+        this.preco = p_preco ;
+
+    }
+}
